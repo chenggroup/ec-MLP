64a7f74c59b872094f43364b95595f6aafd913e2
hans <hanandchen0110@icloud.com>
2025-11-14 12:55:54 +0800

    Add support for pppm/dplr with verlet/split/dplr and correct self-energy computation for non-pppm/dplr k-space styles.

--- src/lmp/verlet_split_dplr.cpp
+++ src/lmp/verlet_split_dplr.cpp
@@ -38,11 +38,17 @@
 #include "modify.h"
 #include "timer.h"
 #include "memory.h"
+#include "math_const.h"
 #include "error.h"
+#include "utils.h"
 
+#include <vector>
 #include "fix_dplr.h"
+#include "pppm_dplr.h"
 
 using namespace LAMMPS_NS;
+using namespace MathConst;  
+using std::vector;          
 
 /* ---------------------------------------------------------------------- */
 
@@ -138,15 +144,25 @@ void VerletSplitDPLR::setup(int flag)
     auto fix_dplr_list = modify->get_fix_by_style("dplr");
     if (fix_dplr_list.size() != 1) error->all(FLERR, "fix dplr should be used once");
     FixDPLR *fix_dplr = (FixDPLR *)fix_dplr_list[0];
-    for (int i = 0; i < nlocal; i++) {
-      fix_dplr->dfele[i * 3 + 0] = atom->f[i][0] - tmp_f[i * 3 + 0];
-      fix_dplr->dfele[i * 3 + 1] = atom->f[i][1] - tmp_f[i * 3 + 1];
-      fix_dplr->dfele[i * 3 + 2] = atom->f[i][2] - tmp_f[i * 3 + 2];
-      atom->f[i][0] = tmp_f[i * 3 + 0];
-      atom->f[i][1] = tmp_f[i * 3 + 1];
-      atom->f[i][2] = tmp_f[i * 3 + 2];
+    /*if (comm->me == 0 && screen) {
+    fprintf(screen, "1\n");
+    fflush(screen);  
+    }*/
+
+    if(force->kspace_match("pppm/dplr", 1) == nullptr) {
+      for (int i = 0; i < nlocal; i++) {
+        fix_dplr->dfele[i * 3 + 0] = atom->f[i][0] - tmp_f[i * 3 + 0];
+        fix_dplr->dfele[i * 3 + 1] = atom->f[i][1] - tmp_f[i * 3 + 1];
+        fix_dplr->dfele[i * 3 + 2] = atom->f[i][2] - tmp_f[i * 3 + 2];
+        atom->f[i][0] = tmp_f[i * 3 + 0];
+        atom->f[i][1] = tmp_f[i * 3 + 1];
+        atom->f[i][2] = tmp_f[i * 3 + 2];
+      }
     }
-    
+    /*if (comm->me == 0 && screen) {
+    fprintf(screen, "2\n");
+    fflush(screen);  
+    }*/
     modify->setup_pre_reverse(eflag,vflag);
     if (force->newton) comm->reverse_comm();
 
@@ -164,13 +180,32 @@ void VerletSplitDPLR::setup(int flag)
 
 void VerletSplitDPLR::k2r_comm()
 {
-  if (eflag) MPI_Bcast(&force->kspace->energy, 1, MPI_DOUBLE, 0, block);
-  if (vflag) MPI_Bcast(force->kspace->virial, 6, MPI_DOUBLE, 0, block);
+
+
 
   int n = 0;
   if (!master) n = atom->nlocal;
+  if(force->kspace_match("pppm/dplr", 1) != nullptr) {
+    PPPMDPLR *pppm_dplr = (PPPMDPLR *)force->kspace_match("pppm/dplr", 1);
+    double *fe = &pppm_dplr->get_fele()[0];
+    //const vector<double> &fe(pppm_dplr->get_fele());
+    //MPI_Gatherv(fe, n*3, MPI_DOUBLE, f_kspace[0], xsize, xdisp,
+    //          MPI_DOUBLE, 0, block);
+    if (master) {
+      int nlocal = atom->nlocal;
+      for (int i = 0; i < nlocal * 3; i++) {
+        fe[i] = 0.0;
+      }
+    }
+    MPI_Gatherv(fe, n*3, MPI_DOUBLE, f_kspace[0], xsize, xdisp,
+              MPI_DOUBLE, 0, block);
+    //utils::logmesg(lmp, "DPLR self energy correction: {:.8f}\n",fe.data()[0]);
+    //MPI_Gatherv(const_cast<double*>(fe.data()), n*3, MPI_DOUBLE, f_kspace[0], xsize, xdisp,
+    //          MPI_DOUBLE, 0, block);
+  }else{
   MPI_Gatherv(atom->f[0], n*3, MPI_DOUBLE, f_kspace[0], xsize, xdisp,
               MPI_DOUBLE, 0, block);
+  }
 
   // Set the forces to FixDPLR object
   auto fix_dplr_list = modify->get_fix_by_style("dplr");
@@ -180,9 +215,67 @@ void VerletSplitDPLR::k2r_comm()
   if (master) {
     int nlocal = atom->nlocal;
     for (int i = 0; i < nlocal; i++) {
+      //utils::logmesg(lmp, "DPLR self energy correction: {:.8f}\n",f_kspace[i][0]);
       fix_dplr->dfele[i * 3 + 0] = f_kspace[i][0];
       fix_dplr->dfele[i * 3 + 1] = f_kspace[i][1];
       fix_dplr->dfele[i * 3 + 2] = f_kspace[i][2];
     }
+  }else{
+      // modify self energy contribution
+      if(force->kspace_match("pppm/dplr", 1) == nullptr){
+        modify_dplr_self_energy_contribution(eflag);
+      
+      }
+  }
+  if (eflag) MPI_Bcast(&force->kspace->energy, 1, MPI_DOUBLE, 1, block);
+  if (vflag) MPI_Bcast(force->kspace->virial, 6, MPI_DOUBLE, 1, block);
+}
+void VerletSplitDPLR::modify_dplr_self_energy_contribution(int eflag)
+{
+  
+  if (eflag & ENERGY_GLOBAL) {
+    
+    KSpace* kspace_fast = force->kspace;
+    if (!kspace_fast) return;
+
+    
+    double qsum_local = 0.0;
+    double qsqsum_local = 0.0;
+
+    for (int i = 0; i < atom->nlocal; i++) {
+      qsum_local += atom->q[i];
+      qsqsum_local += atom->q[i]*atom->q[i];
+    }
+
+    double qsum, qsqsum;
+    MPI_Allreduce(&qsum_local, &qsum, 1, MPI_DOUBLE, MPI_SUM, world);
+    MPI_Allreduce(&qsqsum_local, &qsqsum, 1, MPI_DOUBLE, MPI_SUM, world);
+
+    double g_ewald = kspace_fast->g_ewald;
+
+    //get volume
+    double *prd;
+    int triclinic = domain->triclinic;
+    if (triclinic == 0) prd = domain->prd;
+    else prd = domain->prd_lamda;
+
+    double xprd = prd[0];
+    double yprd = prd[1];
+    double zprd = prd[2];
+
+    // KSpace slab_volfactor
+    double slab_volfactor = kspace_fast->slab_volfactor;
+    double zprd_slab = zprd * slab_volfactor;
+    double volume = xprd * yprd * zprd_slab;
+    
+    // Test: which situations  scale != 1
+    double scale = 1;
+    double qscale = force->qqrd2e * scale;
+
+    double self_energy = (g_ewald*qsqsum/MY_PIS +
+                         MY_PI2*qsum*qsum/(g_ewald*g_ewald*volume)) * qscale;
+    
+    //force->kspace->energy 
+    force->kspace->energy += self_energy;
   }
 }
